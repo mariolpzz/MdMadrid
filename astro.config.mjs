c9a98169000c158042a68ade7510e5a52bc860c7
Mario López <lmario846@gmail.com>
2024-04-06 13:26:12 +0200

    remove import netlify from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
-import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
