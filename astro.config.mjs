cc6137515a586a8da8f17e44ce4d7082c89f93ce
Mario López <lmario846@gmail.com>
2024-04-06 13:36:06 +0200

    manual install netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
+import netlify from '@astrojs/netlify';
+
 
 // https://astro.build/config
 export default defineConfig({
